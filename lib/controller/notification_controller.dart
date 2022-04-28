dfb54e7b981fe156a72385f98fcc7af56b6fecfe
seohyoju <kinnylovemy@gmail.com>
2022-04-28 18:55:28 +0900

    firebase-error 발생

--- lib/controller/notification_controller.dart
+++ lib/controller/notification_controller.dart
@@ -1,5 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 
 class NotificationController extends GetxController{
+  static NotificationController get to => Get.find();
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+  static String msg = '';
 
+  //permission과 configue를 셋팅해야딘다.
+  @override
+  void onInit() {
+    _initNotification();
+    super.onInit();
+  }
+
+  void _initNotification() async{
+    //권한 설정
+    NotificationSettings settings = await _messaging.requestPermission(
+      provisional: true,
+    );
+    print('User granted permission: ${settings.authorizationStatus}');
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
+  // Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
+  //   await Firebase.initializeApp();
+  //   print('handing background message: ${message.messageId}');
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:fcmtest/binding/init_binding.dart';
+import 'package:fcmtest/controller/notification_controller.dart';
 import 'package:fcmtest/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();  //비동기 안정성을 위해 파이어베이스 사용전 초기화 해줌
+  await Firebase.initializeApp(); //controller에서 해줌
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
+  //await Firebase.initializeApp();
+  print('handing background message: ${message.messageId}');
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
